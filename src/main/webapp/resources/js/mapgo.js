94dac5ee851f9c106ccb5997706c76a9b87bda94
hywooKim <khu12687>
2022-04-05 12:41:55 +0900

    main 수정

---
+++ src/main/webapp/resources/js/mapgo.js
@@ -0,0 +1,23 @@
+var map;
+function initMap() {
+  	
+	map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 8,
+      center: { lat: 36.321655 ,lng: 127.378953 }
+    });
+
+  const marker = new google.maps.Marker({
+    position: { lat: 36.321655 ,lng: 127.378953 },
+    map: map,
+    label: "대전 워케이션"
+  });
+  
+	const infowindow = new google.maps.InfoWindow({
+		content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+	});
+	google.maps.event.addListener(marker, "click", () => {
+		infowindow.open(map, marker);
+	});
+  
+}
